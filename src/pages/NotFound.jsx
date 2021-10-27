8ba6b6d221639e4d826130ee7d5d944bbdbc43fe
Roqueheli <roqueheliarenas@gmail.com>
2021-10-26 21:39:18 -0300

    notfound change

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import '../App.css'
 
 function NotFound() {
     return (
         <div className="notfound_container">
             <img src="../../assets/images/404-notfound.gif" alt="" />
-            <Link className="link_style" to="/">Vamos al inicio</Link>
+            <Link className="link_style" to="/">
+                <img src="https://img.icons8.com/ios/35/ffffff/home-page.png" alt="home" />
+            </Link>
         </div>
     )
 };
